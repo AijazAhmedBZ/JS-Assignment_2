50a8f9f75cd845392fa3e6c2129c2adf78b5c89d
Aijaz Ahmed <aijaz.ahmed.bz@gmail.com>
2023-03-05 09:13:14 +0500

    updated on 05-Mar-23

---
+++ JS_2_task_6.js
@@ -0,0 +1,6 @@
+var inPut = prompt("Please enter any number to find even or odd");
+if (inPut % 2 == 0) {
+    document.write("Your input number is even")    
+} else {
+    document.write("Your input number is odd")
+}
---
+++ JS_2_task_7.js
@@ -0,0 +1,9 @@
+var firstNumber = prompt("Please enter first number: ");
+var secondNumber = prompt("Please enter second number: ");
+var operator = prompt("Please input operator i.e. +, - x, / or %: ");
+if (operator == ) {
+var result = firstNumber + secondNumber
+document.write(result);
+} else {
+    document.write("Your input number is odd")
+}
